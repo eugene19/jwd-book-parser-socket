fa56e27f6d72b04fa6a702ea4d2b6f46bf2521fd
degtyaryov <Degtyarov96@mail.ru>
2020-07-28 20:26:45 +0300

    Add ServiceFactory

--- src/main/java/by/epamtc/degtyarovea/controller/Main.java
+++ src/main/java/by/epamtc/degtyarovea/controller/Main.java
@@ -2,11 +2,12 @@ package by.epamtc.degtyarovea.controller;
 
 import by.epamtc.degtyarovea.entity.TextComponent;
 import by.epamtc.degtyarovea.service.BookService;
+import by.epamtc.degtyarovea.service.BookServiceFactory;
 
 public class Main {
 
     public static void main(String[] args) {
-        BookService service = new BookService();
+        BookService service = BookServiceFactory.getInstance().getBookService();
         TextComponent textComponent;
 
         textComponent = service.replaceWordsConcreteLengthInSentence(4, 2, "***");
