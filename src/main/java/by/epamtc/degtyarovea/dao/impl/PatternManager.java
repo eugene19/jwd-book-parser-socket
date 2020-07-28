c7100cd6ab56291bb239e124ffbbc2aed46d5972
degtyaryov <Degtyarov96@mail.ru>
2020-07-28 20:49:44 +0300

    Move patterns.properties in resources file

--- src/main/java/by/epamtc/degtyarovea/dao/impl/PatternManager.java
+++ src/main/java/by/epamtc/degtyarovea/dao/impl/PatternManager.java
@@ -2,11 +2,12 @@ package by.epamtc.degtyarovea.dao.impl;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
 public class PatternManager {
 
-    private static final String PATTERNS_FILEPATH = "resources/patterns.properties";
+    private static final String PATTERNS_FILENAME = "patterns.properties";
     private static final PatternManager instance = new PatternManager();
 
     private String paragraph = ".+\\n*";
@@ -14,9 +15,12 @@ public class PatternManager {
     private String lexeme = "(([-\\w]+)|([-.=()'%,\":;])|(\\s+))";
 
     private PatternManager() {
+        ClassLoader loader = getClass().getClassLoader();
+        String filePath = Objects.requireNonNull(loader.getResource(PATTERNS_FILENAME)).getFile();
         Properties properties = new Properties();
+
         try {
-            properties.load(new FileInputStream(PATTERNS_FILEPATH));
+            properties.load(new FileInputStream(filePath));
             paragraph = properties.getProperty("paragraph.pattern");
             sentence = properties.getProperty("sentence.pattern");
             lexeme = properties.getProperty("lexeme.pattern");
